4e611de3af3c702060f4d7bc215926b5b10fae0d
Andrew <m_andrei71@yahoo.com>
2020-04-23 10:58:51 +0300

    Facade

---
+++ MagazinDeMobila/Facade/Sold.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagazinDeMobila.Facade
+{
+    class Sold
+    {
+        public double CurrentSold { get; set; }
+        public Sold(double currentSold)
+        {
+            CurrentSold += currentSold;
+        }
+        public void addMoney(double sum)
+        {
+            CurrentSold += sum;
+        }
+        public void removeMoney(double sum)
+        {
+            if (CurrentSold > sum)
+                CurrentSold -= sum;
+            else
+                Console.WriteLine("We dont have that kind of money in our current sold");
+        }
+        public double getSold()
+        {
+            return CurrentSold;
+        }
+    }
+}
